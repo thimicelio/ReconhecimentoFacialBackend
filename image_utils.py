96efe42c4b767d81afb431fa89f0c89842d3f735
Leonardo Moura <leosmsilvx@gmail.com>
2026-04-23 20:13:56 -0300

    Melhoria no preprocessamento da imagem

--- image_utils.py
+++ image_utils.py
@@ -4,11 +4,13 @@ import cv2
 def preprocessar_imagem(caminho):
     img = cv2.imread(caminho)
 
-    altura_max = 1000
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    h, w = img.shape[:2]
-    if h > altura_max:
-        proporcao = altura_max / h
-        img = cv2.resize(img, (int(w * proporcao), altura_max))
+    img = cv2.equalizeHist(img)
+
+    img = cv2.GaussianBlur(img, (3, 3), 0)
+
+    # cv2.imwrite("saida.png", img) # DEBUG ONLY
 
     return img
+
